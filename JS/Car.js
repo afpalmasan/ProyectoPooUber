d2ad253c3006dc7a761b53e8df21bd7641d3ca41
Andres Felipe Palma <afpalma21@gmail.com>
2023-04-14 21:37:12 -0500

    Commit js

---
+++ JS/Car.js
@@ -0,0 +1,8 @@
+class Car{
+    constructor(id,license,driver,passenger){
+        this.id = id;
+        this.license = license;
+        this.driver = driver;
+        this.passenger = passenger;
+    }
+}
